1281811e6131bf4f34ebc9cecec9f3adf4747d3f
Greg <retgunner@gmail.com>
2023-02-24 14:05:59 +0300

    wtf is char*

--- ex04/ft_is_negative.c
+++ ex04/ft_is_negative.c
@@ -3,18 +3,16 @@
 void ft_is_negative(int n);
 
 int main(void) {
-    int m;
-    ft_is_negative(m);
+    ft_is_negative(-1);
+    ft_is_negative(0);
+    ft_is_negative(1);
 }
 
 void ft_is_negative(int n) {
-    char c;
     if (n > 0) {
-        c = 'N';
-        write(1, &c, 1);
+        write(1, "P", 1);
     }
     else {
-        c = 'P';
-        write(1, &c, 1);
+        write(1, "N", 1);
     }
 }
