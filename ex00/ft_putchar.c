741ddaf991fda6e997b474043e0fa34ffe6a0b2b
Nanonypl <v.turushev@gmail.com>
2023-02-24 03:24:52 +0300

    1234 23

---
+++ ex00/ft_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+void ft_putchar(char c);
+
+int main(void) {
+    ft_putchar('\n');
+    ft_putchar('o');
+}
+
+void ft_putchar(char c) {
+    // ssize_t write(int fd, const void *buf, size_t count);
+    write(1, &c, 1);
+}
